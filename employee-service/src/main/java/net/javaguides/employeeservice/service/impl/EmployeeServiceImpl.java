b8dfbaadef494215eac03ed8fa1fd1045e7a7866
Rohit <nathrohit48@gmail.com>
2025-05-27 00:55:48 +0530

    Initial commit for two microservice created 'department-service' and 'employee-service'

---
+++ employee-service/src/main/java/net/javaguides/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,34 @@
+package net.javaguides.employeeservice.service.impl;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.employeeservice.dto.EmployeeDto;
+import net.javaguides.employeeservice.entity.Employee;
+import net.javaguides.employeeservice.mapper.EmployeeMapper;
+import net.javaguides.employeeservice.repository.EmployeeRepository;
+import net.javaguides.employeeservice.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
+        Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+
+        Employee saveDEmployee = employeeRepository.save(employee);
+
+        EmployeeDto savedEmployeeDto = EmployeeMapper.mapToEmployeeDto(saveDEmployee);
+
+        return savedEmployeeDto;
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId).get();
+        EmployeeDto employeeDto = EmployeeMapper.mapToEmployeeDto(employee);
+        return employeeDto;
+    }
+}
