c783959e58360846dba5fda5ba961087ac129648
Rohit <nathrohit48@gmail.com>
2025-06-01 11:41:33 +0530

    REST API Documentation for 'Employee-service' using 'SpringDoc OpenAPI' for Swagger UI

--- employee-service/src/main/java/net/javaguides/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/net/javaguides/employeeservice/EmployeeServiceApplication.java
@@ -1,5 +1,10 @@
 package net.javaguides.employeeservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -7,6 +12,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Employee Service REST APIs",
+				description = "Employee Service REST APIs Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Rohit",
+						email = "nathrohit48@gmail.com",
+						url = "javaguides.net"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "javaguides.net/license"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Employee-Service Doc",
+				url = "javaguides.net/employee_Service.html"
+		)
+)
 @SpringBootApplication
 @EnableFeignClients
 public class EmployeeServiceApplication {
